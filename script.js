01a50f4cb9a68c0889df8feaacfa0ee16ff8f693
NowrinIslamMim <nowrin02@gmail.com>
2022-09-14 23:09:16 +0600

    mistakes count fixed

--- script.js
+++ script.js
@@ -46,6 +46,7 @@ const typeController = (e) => {
     display.innerHTML += `<span class="green">${newLetter === " " ? "▪" : newLetter}</span>`;
   } else {
     display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`;
+
   }
 
   // check if given question text is equal to user typed text
@@ -58,6 +59,7 @@ const validate = (key) => {
   if (key === questionText[userText.length - 1]) {
     return true;
   }
+  errorCount = errorCount + 1;
   return false;
 };
 
